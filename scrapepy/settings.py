a45f5b59448e66d9232f9c610bc81279a5e37ac8
ivanpilipovic <46201857+ivanpilipovic@users.noreply.github.com>
2019-09-05 10:55:54 +0200

    Update settings.py

--- scrapepy/settings.py
+++ scrapepy/settings.py
@@ -10,9 +10,6 @@ OS_ENV = "windows" if p_os == "win32" else "osx" if p_os == "darwin" else "linux
 
 
 class Settings:
-    """ Globally accessible settings throughout whole project """
-    log_location = os.path.join(BASE_DIR, 'logs')
-    database_location = os.path.join(BASE_DIR, 'db', 'instapy.db')
 
     chromedriver_min_version = 2.36
 
@@ -21,30 +18,3 @@ class Settings:
 
     if not os.path.exists(chromedriver_location):
         chromedriver_location = os.path.join(BASE_DIR, 'assets', 'chromedriver')
-
-    loggers = {}
-    logger = None
-
-    profile = {"id": None, "name": None}
-
-    QS_config = {}
-
-    connection_type = None
-
-    action_delays = {}
-
-    meaningcloud_config = {}
-    yandex_config = {}
-
-    # store the parameter for global access
-    show_logs = None
-
-
-
-
-
-class Storage:
-    record_activity = {}
-
-
-
